cfb596b5e31ec77e2ede884c5f28842e68512b6c
anthelme kpodar <tekoanthelmekpodar@gmail.com>
2024-06-11 15:40:56 +0000

    bon restau comit message in one

--- public/assets/auth/js/dist/auth.dev.js
+++ public/assets/auth/js/dist/auth.dev.js
@@ -1,45 +1,39 @@
-"use strict";
+/*function validPassword(password) {
+    // Regex pour vérifier les critères du mot de passe
+    const minLength = 8;
+    const specialCharPattern = /[@!#\$%\^\&*\)\(+=._-]/; // Vous pouvez ajouter d'autres caractères spéciaux si nécessaire
+    const uppercasePattern = /[A-Z]/;
 
-function validPassword(password) {
-  // Regex pour vérifier les critères du mot de passe
-  var minLength = 8;
-  var specialCharPattern = /[@!#\$%\^\&*\)\(+=._-]/; // Vous pouvez ajouter d'autres caractères spéciaux si nécessaire
+    // Vérifier la longueur du mot de passe
+    if (password.length < minLength) {
+        return 'error';
+    }
 
-  var uppercasePattern = /[A-Z]/; // Vérifier la longueur du mot de passe
+    // Vérifier la présence d'un caractère spécial et d'une majuscule
+    if (!specialCharPattern.test(password) || !uppercasePattern.test(password)) {
+        return 'error';
+    }
 
-  if (password.length < minLength) {
-    return 'error';
-  } // Vérifier la présence d'un caractère spécial et d'une majuscule
-
-
-  if (!specialCharPattern.test(password) || !uppercasePattern.test(password)) {
-    return 'error';
-  }
-
-  return 'success';
+    return 'success';
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  var passwordInput = document.querySelector('.signup-form .mb-1 input[type="password"]');
-  var form = document.querySelector('.signup-form');
+document.addEventListener('DOMContentLoaded', () => {
+    const passwordInput = document.querySelector('.signup-form .mb-1 input[type="password"]');
+    const form = document.querySelector('.signup-form');
 
-  if (passwordInput) {
-    passwordInput.addEventListener('input', function () {
-      var result = validPassword(passwordInput.value);
-
-      if (result === 'error') {
-        passwordInput.classList.remove('bottom_border_success');
-        passwordInput.classList.add('bottom_border_error');
-
-        form.onsubmit = function (e) {
-          return e.preventDefault();
-        }; // Empêche l'envoi du formulaire
-
-      } else {
-        passwordInput.classList.remove('bottom_border_error');
-        passwordInput.classList.add('bottom_border_success');
-        form.onsubmit = null; // Autorise l'envoi du formulaire
-      }
-    });
-  }
-});
+    if (passwordInput) {
+        passwordInput.addEventListener('input', () => {
+            const result = validPassword(passwordInput.value);
+            if (result === 'error') {
+                passwordInput.classList.remove('bottom_border_success');
+                passwordInput.classList.add('bottom_border_error');
+                form.onsubmit = (e) => e.preventDefault(); // Empêche l'envoi du formulaire
+            } else {
+                passwordInput.classList.remove('bottom_border_error');
+                passwordInput.classList.add('bottom_border_success');
+                form.onsubmit = null; // Autorise l'envoi du formulaire
+            }
+        });
+    }
+});*/
+"use strict";
