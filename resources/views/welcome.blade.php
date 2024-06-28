cc457860dc09a9d6bc6e917ad02eb3cf4987985c
anthelme kpodar <tekoanthelmekpodar@gmail.com>
2024-06-28 05:33:10 +0000

    home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
 <link rel="stylesheet" href="{{asset('assets/css/home.css')}}?v=<?php echo time(); ?>">
 <div class="container">
     <div class="content">
-        <h1 class="bg-span">BIENVENUE DANS VOTRE APLLICATION DE GESTION DES BONS DE RESTAURANT</h1>
+        <h1 class="bg-span">BIENVENUE DANS VOTRE APPLICATION DE GESTION DES BONS DE RESTAURANT</h1>
         <div class="row row-cols-1 row-cols-md-3 g-1">
             <div class="col">
                 <div class="card">
@@ -70,6 +70,8 @@
 
         </div>
         <x-list-scroll/>
+        <x-rotatings-cards />
+
         <hr />
         @include('layouts.footer')
     </div>
