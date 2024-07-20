6211a0860e7302f7e2ae0a0dbe0709345d63d810
anthelme kpodar <tekoanthelmekpodar@gmail.com>
2024-07-20 15:32:42 +0000

    fixe

--- app/Http/Services/LinkService.php
+++ app/Http/Services/LinkService.php
@@ -10,8 +10,9 @@ class LinkService
     public function __construct(LinkRepository $linkRepository){
       $this->linkRepo=$linkRepository;
     }
-    public function create(array $data){
-        $expire=$data['time_expire'];
+    public function create(array $data)
+    {
+        $expire = $data['time_expire'];
       
         $timeExpire = match ($expire) {
             '24h' => Carbon::now()->addDay(),
@@ -19,30 +20,35 @@ class LinkService
             '1month' => Carbon::now()->addMonth(),
             default => Carbon::now()->addDay(),
         };
-        //dd($timeExpire);
-
-        $auth=Auth()->user()->referral_code;
-        $exist=$this->linkRepo->getByAuthCode($auth);
-        if($exist && !$exist->expire){
-            return redirect()->back()
-            ->with('warning','Vous aviez deja génerer un lien et il est toujours valide')
-            ->with('link',$exist->link);
-        }else{
-            $data['time_expire']=$timeExpire;
-            $data['auth_code']=$auth;
-            $data['link']=$this->generateLink($auth);
+    
+        $auth = auth()->user()->referral_code;
+        $link = $this->generateLink($auth);
+        $existingLink = $this->linkRepo->getByAuthCode($auth);
+    
+        if ($existingLink && !$existingLink->expire) {
+            if ($existingLink['link'] !== $link) {
+                // Le lien généré n'est pas sur la bonne adresse actuelle
+                $existingLink['link'] = $link;
+                $existingLink->save();
+            }
+    
+            return redirect()->back()->with('link', $existingLink->link);
+        } else {
+            $data['time_expire'] = $timeExpire;
+            $data['auth_code'] = $auth;
+            $data['link'] = $link;
             $this->linkRepo->create($data);
-            return redirect()->back()->with('link',$data['link']);
+    
+            return redirect()->back()->with('link', $data['link']);
         }
-       
-
     }
+    
     public function generateLink($auth)
     {
         $uniqueCode = $auth;
-       // $https = 'https://greendetox.senousolutions.com/sign-up?code=' . $uniqueCode;
-       // $http=  'http://localhost:8000/sign-up?code=' . $uniqueCode; 
-       $url=route('sign-up').'?code='.$uniqueCode;
-        return $url;   
+        $url = route('sign-up') . '?code=' . $uniqueCode;
+    
+        return $url;
     }
+    
 }
