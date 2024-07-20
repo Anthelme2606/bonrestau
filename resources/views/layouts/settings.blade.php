d4c611ff43e318dd994edb3a84e3561120043122
anthelme kpodar <tekoanthelmekpodar@gmail.com>
2024-07-20 18:26:26 +0000

    fixe

--- resources/views/layouts/settings.blade.php
+++ resources/views/layouts/settings.blade.php
@@ -488,7 +488,7 @@
             @foreach ($users as $user)
             @if ($currentLevel < $maxLevels)
                 <div class="level">
-                   
+                   {{dd($user->referrals,$user)}}
                     @if ( $user && $user->referrals->count() <= 6)
                     
                         @foreach ($user->referrals as $referral)
