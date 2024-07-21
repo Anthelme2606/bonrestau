c771f43eadbe20bbad63fe96296887378d7d7b9f
anthelme kpodar <tekoanthelmekpodar@gmail.com>
2024-07-21 09:54:25 +0000

    fixe

--- resources/views/layouts/html.blade.php
+++ resources/views/layouts/html.blade.php
@@ -4,6 +4,12 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta property="og:title" content="GREEN DETOX">
+    <meta property="og:description" content="Rejoignez notre communauté.">
+    <meta property="og:image" content="{{ asset('favicon.ico') }}">
+    <meta property="og:url" content="{{ route('home') }}">
+    <meta property="og:type" content="website">
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="google-site-verification" content="XlL6Hde6-hJL2HiYzL6ZvmvO6hfxLShfCHASRxhIUrc" />
     <title>{{ config('app.name') }} | @yield('title')</title>
@@ -17,17 +23,17 @@
     @endif
     <!--CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-   
+
 </head>
 
 <body style="background:rgba(245,251,252,1)">
-    
+
     <div class="content-global">
         @yield('body-container')
     </div>
-    <x-spinner/>
-    
-    
+    <x-spinner />
+
+
     <x-toastr />
 
 </body>
